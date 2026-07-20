81d984e26e4529e2a69120b6f68aa38eff0ee8b7
LIN <qwaszx1234224@gmail.com>
2026-07-20 11:13:30 +0800

    fix1 架構 1.example-service docker啟動參數 (未測試)

--- infrastructure/mariadb/init-db.sql
+++ infrastructure/mariadb/init-db.sql
@@ -1,11 +1,13 @@
 CREATE DATABASE IF NOT EXISTS user_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 CREATE DATABASE IF NOT EXISTS order_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CREATE DATABASE IF NOT EXISTS example_db CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- Create the user if it does not exist before granting permissions
 CREATE USER IF NOT EXISTS 'ai4dt123'@'%' IDENTIFIED BY 'Aa0000';
 
 GRANT ALL PRIVILEGES ON user_db.* TO 'ai4dt123'@'%';
 GRANT ALL PRIVILEGES ON order_db.* TO 'ai4dt123'@'%';
+GRANT ALL PRIVILEGES ON example_db.* TO 'ai4dt123'@'%';
 
 FLUSH PRIVILEGES;
 
